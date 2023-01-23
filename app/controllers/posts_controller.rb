e1dd4e9e992d349577a0b43460c93547f136c2b0
kasumi sogabe <ksm17sgb@gmail.com>
2023-01-23 16:12:16 +0900

    Strong Parametersを使ってcreateアクションの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
